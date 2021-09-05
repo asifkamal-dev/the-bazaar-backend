05d0a333b55207b2144ef392fb5b69319d652e60
Asif Kamal <79679333+asifk87@users.noreply.github.com>
2021-09-05 17:27:56 -0500

    models added and happy that the order staging(aka ordeitem model works the way i want, and ordered is the final order. Might have to add a unique hash to the order to represent transaction number

--- bazaar/admin.py
+++ bazaar/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Category, Product, Order, OrderItem
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display= ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['category','created_by','name','slug','price','description','image','in_stock','created','updated','is_active']
+    list_filter = ['in_stock','is_active']
+    list_editable = ['price','in_stock','is_active']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Order)
+admin.site.register(OrderItem)
