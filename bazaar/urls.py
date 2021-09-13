6a417be66da260a7da46074e99cab87925869d6c
Asif Kamal <79679333+asifk87@users.noreply.github.com>
2021-09-13 08:27:52 -0500

    just pushing up latests changes

--- bazaar/urls.py
+++ bazaar/urls.py
@@ -6,7 +6,7 @@ app_name= 'bazaar'
 
 urlpatterns = [
     path('products/', views.ProductList.as_view(), name='product_list'),
-    path('products/<int:pk>', views.ProductDetail.as_view(), name='product_detail'),
+    path('product/<int:pk>', views.ProductDetail.as_view(), name='product_detail'),
     path('categories/', views.CategoryList.as_view(), name='category_list'),
     path('category/<int:pk>', views.CategoryDetail.as_view(), name='category_detail'),
     path('basket/', views.BasketList.as_view(), name='basket_list'),
