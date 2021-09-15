7af96725ec76809333fa89398047c938e6ee9b3d
Asif Kamal <79679333+asifk87@users.noreply.github.com>
2021-09-15 18:36:30 -0500

    BACKEND READY FOR HEROKU, this is before i bring in react to test

--- backend/settings.py
+++ backend/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-87$tpkm=#mv$6yum1ycc%2b&j+7$m^s3_-z4^lz*ve_0_^8&q@'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ['MODE'] == 'dev' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -57,15 +58,18 @@ REST_FRAMEWORK = {
 #     ]
 # }
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-]
-# CORS_ALLOWED_ORIGINS = [
+# CORS_ORIGIN_WHITELIST = [
 #     'http://localhost:3000',
 # ]
+# # CORS_ALLOWED_ORIGINS = [
+# #     'http://localhost:3000',
+# # ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -100,13 +104,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bazaar',
-        'USER': 'bazaaruser',
-        'PASSWORD': 'bazaar',
-        'HOST': 'localhost'
-    }
+    'default':dj_database_url.config(conn_max_age=600)
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'bazaar',
+    #     'USER': 'bazaaruser',
+    #     'PASSWORD': 'bazaar',
+    #     'HOST': 'localhost'
+    # }
 }
 
 
@@ -146,7 +151,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR,'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
